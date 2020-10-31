b2dd62ec220586f12e963996450fea34e4f0d58e
Josh Broomberg <joshbroomberg@gmail.com>
2020-10-30 17:27:09 -0700

    persist specific files

--- src/report/validate-best-report.sh
+++ src/report/validate-best-report.sh
@@ -1,2 +1,2 @@
 jupyter nbconvert --to html 'notebooks/validate-best.ipynb' --TemplateExporter.exclude_input=True --execute
-mv -f notebooks/validate-best.html src/report/generated/validate_best_report.html
+mv -f notebooks/validate-best.html models/reports/validate_best_report.html
